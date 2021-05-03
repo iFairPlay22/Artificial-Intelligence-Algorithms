044630d66b81267aeb3cda0e812068a127dbeb1b
Pholith <vincent22062@gmail.com>
2021-05-03 13:23:41 +0200

    IA morpion trop forte

--- Sujet 3/Morpion.py
+++ Sujet 3/Morpion.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 import random
 import numpy as np
+import tkinter.font as tkFont # ou «import TkFont» pour python 2
 
 ###############################################################################
 # création de la fenetre principale  - ne pas toucher
@@ -47,8 +48,8 @@ canvas.place(x=0,y=0)
 #
 #  Parametres du jeu
  
-Grille = [ [0,0,1], 
-           [2,0,0], 
+Grille = [ [0,0,0], 
+           [0,0,0], 
            [0,0,0] ]  # attention les lignes représentent les colonnes de la grille
            
 Grille = np.array(Grille)
@@ -60,21 +61,130 @@ Grille = Grille.transpose()  # pour avoir x,y
 #
 # gestion du joueur humain et de l'IA
 # VOTRE CODE ICI 
+DebutDePartie = False
+vainqueur = 0
 
-def Play(x,y):             
-    Grille[x][y] = 1
-   
-          
+def TestPartieFini():
+    global Grille, vainqueur
+    vainqueur = 0
+    # Tests fin de partie
+    for player in [1, 2]:
+        # Check les colonnes
+        for colonne in Grille:
+            if all(cell == player for cell in colonne):
+                vainqueur = player
+
+        # Check les lignes
+        for i in range(len(Grille)):
+            if all(Grille[j][i] == player for j in range(len(Grille[0]))):
+                vainqueur = player
+        
+        # Check les croix
+        croix = [0, 1, 2]
+        if all(Grille[i][i] == player for i in croix):
+            vainqueur = player
+
+        if all(Grille[i][len(Grille[i]) - 1 - i] == player for i in croix):
+            vainqueur = player
+
+    if vainqueur == 0 and all(Grille[i][j] != 0 for i in range(len(Grille)) for j in range(len(Grille[i]))):
+        vainqueur = 3
+
+def Play(x,y):
+    global DebutDePartie, Grille, vainqueur
+
+    if DebutDePartie or vainqueur != 0:
+        Grille = [ [0,0,0], 
+                   [0,0,0], 
+                   [0,0,0] ]  # attention les lignes représentent les colonnes de la grille
+        vainqueur = 0
+        DebutDePartie = False
+        Dessine()
+        return
+
+    if Grille[x][y] != 0:
+        return
     
+    # Placement du joueur
+    Grille[x][y] = 1
+
+    TestPartieFini()
+    if (vainqueur != 0): return
+    print("Chargement du coup de l'IA ...")
+    result, coup = JoueurSimulePlayerN(2)
+    print("coups possibles = ", CoupsPossibles())
+    print("coup choisi= ", result, coup)
+    Grille[coup[0]][coup[1]] = 2
+    TestPartieFini()
+    if (vainqueur != 0): return
+
+
+# Liste qui contient des tuples de coups, avec x et y
+def CoupsPossibles():
+    coupsPossibles = []
+
+    for i in range(len(Grille)):
+        for j in range(len(Grille[i])):
+            if Grille[i][j] == 0:
+                coupsPossibles.append((i, j))
+    return coupsPossibles
+
+
+# coup est un tuple comme ça (x, y)
+# result est une liste comme ça [(result, coup), ...]
+# les retours des fonctions sont comme ça (result, coup)
+
+def JoueurSimulePlayerN(playerN):
+    if vainqueur != 0:
+        return (vainqueur, (-1, -1))
+    
+    result = []
+    coups = CoupsPossibles()
+    
+    for coup in coups:
+        Grille[coup[0]][coup[1]] = playerN
+        TestPartieFini()
+
+        #Change le joueur qui appelle la fonction
+        nextPlayer = 1
+        if playerN == 1: nextPlayer = 2
+        
+        r = JoueurSimulePlayerN(nextPlayer)
+        TestPartieFini()
+
+        result.append((r[0], coup))
+        Grille[coup[0]][coup[1]] = 0
+
+
+    if playerN == 1:
+        pass
+        #print("Calcul coups IA: ", result)
+    # Cherche s'il existe un coup gagnant
+    for result_coup in result:
+        if result_coup[0] == playerN:
+            return result_coup
+    
+    # Cherche s'il existe un coup nul
+    for result_coup in result:
+        if result_coup[0] == 3:
+            return result_coup
+
+    for result_coup in result:
+        if result_coup[0] == 0:
+            return result_coup
+
+    # Coup perdant
+    return result[0]
+
     
 ################################################################################
 #    
 # Dessine la grille de jeu
 
-def Dessine(PartieGagnee = False):
+def Dessine():
         ## DOC canvas : http://tkinter.fdex.eu/doc/caw.html
         canvas.delete("all")
-        
+
         for i in range(4):
             canvas.create_line(i*100,0,i*100,300,fill="blue", width="4" )
             canvas.create_line(0,i*100,300,i*100,fill="blue", width="4" )
@@ -89,7 +199,14 @@ def Dessine(PartieGagnee = False):
                 if ( Grille[x][y] == 2):
                     canvas.create_oval(xc+10,yc+10,xc+90,yc+90,outline="yellow", width="4" )
         
-       
+        if vainqueur != 0:
+            font = tkFont.Font(size=50)
+            textVainqueur = "Gagné"
+            if vainqueur == 2: textVainqueur = "Perdu"
+            if vainqueur == 3: textVainqueur = "Match nul"
+            canvas.create_text(LARG / 2, HAUT / 4, text=textVainqueur, fill="white", font=font)
+        
+
         
   
 ####################################################################################
@@ -110,6 +227,8 @@ def MouseClick(event):
     
     Dessine()
     
+
+
 canvas.bind('<ButtonPress-1>',    MouseClick)
 
 #####################################################################################
